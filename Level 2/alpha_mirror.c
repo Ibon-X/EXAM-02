4250e8fb73cf09be14f6b641f32bfe3ef3c69a68
Ibon-X <108282338+Ibon-X@users.noreply.github.com>
2022-10-10 10:52:14 +0200

    Add files via upload

--- Level 2/alpha_mirror.c
+++ Level 2/alpha_mirror.c
@@ -1,22 +1,22 @@
 #include <unistd.h>
 
-int main(int ac,char **av)
+void	alpha_mirror(char *s)
 {
-	int i;
-
-	if (ac == 2)
-	{	
-		i = 0;
-		while (av[1][i])
-		{
-			if (av[1][i] >= 'A' && av[1][i] <= 'Z')
-				av[1][i] = 'Z' - av[1][i] + 'A';
-			else if (av[1][i] >= 'a' && av[1][i] <= 'z')
-				 av[1][i] = 'z' - av[1][i] + 'a';
-			write (1, &av[1][i], 1);
-			i++;
-		}
+	while (*s)
+	{
+		if (*s >= 'A' && *s <= 'Z')
+			*s = 'Z' - *s + 'A';
+		else if (*s >= 'a' && *s <= 'z')
+			*s = 'z' - *s + 'a';
+		write (1, s, 1);
+		s++;
 	}
+}
+
+int main(int ac, char **av)
+{
+	if (ac == 2)
+		alpha_mirror(av[1]);
 	write (1, "\n", 1);
 	return (0);
 }
