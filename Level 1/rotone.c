18bf1feacfd40ef7e777248f530988997cb0af3a
Ibon-X <108282338+Ibon-X@users.noreply.github.com>
2022-10-11 10:39:34 +0200

    Add files via upload

--- Level 1/rotone.c
+++ Level 1/rotone.c
@@ -1,23 +1,22 @@
 #include <unistd.h>
 
-int main(int ac, char **av)
+void	rotone(char *s)
 {
-	int i;
-
-	if (ac == 2)
+	while (*s)
 	{
-		i = 0;
-		while (av[1][i])
-		{
-			if ((av[1][i] >= 'A' && av[1][i] <= 'Y')
-				|| (av[1][i] >= 'a' && av[1][i] <= 'y'))
-				av[1][i] += 1;
-			else if (av[1][i] == 'Z' || av[1][i] == 'z')
-				av[1][i] -= 25;
-			write (1, &av[1][i], 1);
-			i++;
-		}
+		if ((*s >= 'A' && *s <= 'Y') || (*s >= 'a' && *s <= 'y'))
+			*s += 1;
+		else if (*s == 'Z' || *s == 'z')
+			*s -= 25;
+		write (1, s, 1);
+		s++;
 	}
+}
+
+int 	main(int ac, char **av)
+{
+	if (ac == 2)
+		rotone(av[1]);
 	write (1, "\n", 1);
 	return (0);
 }
