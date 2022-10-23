22aa550c266cd973298eea6964ae9cd6d77d1cef
Ibon-X <108282338+Ibon-X@users.noreply.github.com>
2022-10-23 20:24:43 +0200

    Update ft_range.c

--- Level 3/ft_range.c
+++ Level 3/ft_range.c
@@ -7,20 +7,21 @@ int	absolute_value(int n)
 
 int     *ft_range(int start, int end)
 {
-	int	*res;
 	int	i;
 	int 	range;
+	int	*res;
 	int 	step;
 
+	i = 0;
 	range = 1 + absolute_value(end - start);
 	res = malloc(sizeof(int) * range);
+	
 	if (!res) 
 		return (NULL);
 	if (start < end)
 		step = 1;
 	else
 		step = -1;
-	i = 0;
 	while (range--)
 	{
 		res[i] = start;
