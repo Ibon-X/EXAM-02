00085f9a94e9e165bdd609be05d1452f3a29ca86
Ibon-X <108282338+Ibon-X@users.noreply.github.com>
2022-10-23 20:22:45 +0200

    Add files via upload

---
+++ Level 3/ft_range.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+
+int	absolute_value(int n)
+{
+	if (n < 0)
+		return (-n);
+	return (n);
+}
+
+int     *ft_range(int start, int end)
+{
+	int	*res;
+	int	i;
+	int 	range;
+	int 	step;
+
+	range = 1 + absolute_value(end - start);
+	res = malloc(sizeof(int) * range);
+	if (!res) 
+		return (NULL);
+	if (start < end)
+		step = 1;
+	else
+		step = -1;
+	i = 0;
+	while (range--)
+	{
+		res[i] = start;
+		start += step;
+		i++;
+	}
+	return (res);
+}
